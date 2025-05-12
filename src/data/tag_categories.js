9102abecadf3df08acd69378a1c2ffa5f2bd9fd0
Mark <markfreeth17@yahoo.com>
2025-05-12 14:33:17 +0100

    Updated tags

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -2,16 +2,13 @@ const userDefinedTagCategories = {
 	"Paper Type/Focus": {
 		"description": "The type/focus of the paper"
 	},
-	"Literature Review/Survey": {
-		"description": "A literature survey/review of XAI in healthcare"
+	"AI Technique Used": {
+		"description": "The AI technique used in the paper"
+	}
+	"XAI Technique Used": {
+		"description": "The XAI technique/s used in the paper"
 	},
-	"Single XAI Technique": {
-		"description": "Papers that apply a single XAI technique to healthcare"
-	},
-	"Multiple/Unified XAI Techniques": {
-		"description": "Papers that apply multiple XAI techniques to healthcare"
-	},
-	"Comparison of XAI Techniques": {
-		"description": "Papers that compare XAI techniques on the same healthcare data"
+	"Data Used": {
+		"description": "The data used in the paper"
 	}
 }
